9a391d8d9c53fc806880745abd13bf4ccdbd8bfc
IrtezaAsadRizvi <irteza.asad.cse@ulab.edu.bd>
2018-10-12 16:57:44 +0600

    Home page: drug update and star product added

--- js/ui.js
+++ js/ui.js
@@ -5,4 +5,21 @@ $(function() {
 	  $(".search-tabs li").removeClass("active");
 	  $(this).addClass("active");
 	});
+
+	$('#home-product-slider').slick({
+	  slidesToScroll: 1,
+		slidesToShow: 3,
+	  autoplay: true,
+	  autoplaySpeed: 1000,
+		infinite: true,
+    arrows: false,
+		responsive: [
+		 {
+			 breakpoint: 480,
+			 settings: {
+				 slidesToShow: 2,
+			 }
+		 }
+	 ]
+	});
 });
